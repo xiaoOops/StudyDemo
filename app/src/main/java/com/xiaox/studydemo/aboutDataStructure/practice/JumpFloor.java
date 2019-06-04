58848d7b9a12ae7dfd10c09634e807c6e684a4af
xiaoxiao <303378520@qq.com>
2019-06-04 20:25:20 +0800

    跳台阶问题

--- app/src/main/java/com/xiaox/studydemo/aboutDataStructure/practice/JumpFloor.java
+++ app/src/main/java/com/xiaox/studydemo/aboutDataStructure/practice/JumpFloor.java
@@ -49,7 +49,7 @@ public class JumpFloor {
 
 
     /**
-     * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法
+     * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上m级（m <= n）求该青蛙跳上一个n级的台阶总共有多少种跳法
      */
     public static int JumpFloor2(int target) {
         if (target <= 0) {
