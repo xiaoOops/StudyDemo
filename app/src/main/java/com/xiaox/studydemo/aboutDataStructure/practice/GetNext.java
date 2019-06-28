73a758460fcb1600bf1252a99a33700c7a54dc5b
xiaoxiao <303378520@qq.com>
2019-06-28 16:43:04 +0800

    二叉树的下一个结点 & 孩子们的游戏(圆圈中最后剩下的数)

--- app/src/main/java/com/xiaox/studydemo/aboutDataStructure/practice/GetNext.java
+++ app/src/main/java/com/xiaox/studydemo/aboutDataStructure/practice/GetNext.java
@@ -12,6 +12,39 @@ public class GetNext {
 
     ArrayList<TreeLinkNode> mTreeLinkNodes = new ArrayList<>();
 
+
+    /**
+     * 1.二叉树为空，则返回空；
+     * 2.节点右孩子存在，则设置一个指针从该节点的右孩子出发，下一个节点就是右子树最左边的点；
+     * 3.节点不是根节点。如果该节点是其父节点的左孩子，则返回父节点；
+     * 否则继续向上遍历其父节点的父节点，重复之前的判断，即直到当前结点是其父节点的左孩子位置，返回结果。
+     */
+    public TreeLinkNode getNext(TreeLinkNode pNode) {
+        if (pNode == null) {
+            return null;
+        }
+        if (pNode.right != null) {
+            //情况2，如果右子树存在，则从右子树出发，找到右子树的左子节点
+            TreeLinkNode temp = pNode.right;
+            while (temp.left != null) {
+                temp = temp.left;
+            }
+            return temp;
+        }
+        //情况3，判断该节点是否为父节点的左子树，不是的话继续向上遍历，重复判断
+        while (pNode.next != null) {
+            if (pNode.next.left == pNode) {
+                return pNode.next;
+            }
+            pNode = pNode.next;
+        }
+        return null;
+    }
+
+
+    /**
+     * 直接中序遍历一遍。。。
+     */
     public TreeLinkNode GetNext(TreeLinkNode pNode) {
         //TreeLinkNode head = pNode.next;
         InOrder(pNode);
@@ -20,11 +53,14 @@ public class GetNext {
         return linkNode;
     }
 
+    /**
+     * 中序遍历二叉树
+     */
     public void InOrder(TreeLinkNode node) {
         if (node != null) {
-            InOrder(node.left);  //中根遍历左子树
+            InOrder(node.left);  //中序遍历左子树
             mTreeLinkNodes.add(node);
-            InOrder(node.right);  //中根遍历右子树
+            InOrder(node.right);  //中序遍历右子树
         }
     }
 
---
+++ app/src/main/java/com/xiaox/studydemo/aboutDataStructure/practice/LastRemaining.java
@@ -0,0 +1,80 @@
+package com.xiaox.studydemo.aboutDataStructure.practice;
+
+/**
+ * @Description: 首先, 让小朋友们围成一个大圈。然后,他随机指定一个数m,让编号为0的小朋友开始报数。每次喊到m-1的那个小朋友要出列唱首歌,
+ * 然后可以在礼品箱中任意的挑选礼物,并且不再回到圈中,从他的下一个小朋友开始,继续0...m-1报数....这样下去....
+ * 直到剩下最后一个小朋友,可以不用表演,并且拿到牛客名贵的“名侦探柯南”典藏版(名额有限哦!!^_^)。
+ * 请你试着想下,哪个小朋友会得到这份礼品呢？(注：小朋友的编号是从0到n-1)
+ * @date 2019/6/28
+ * @author:xiaox
+ */
+public class LastRemaining {
+
+
+    public static void main(String[] arg) {
+        int m = 4;
+        int n = 10;
+        LastRemaining_Solution(n, m);
+    }
+
+
+    /**
+     * 用数组来模拟环,用一个step记录数过的数，数到m就清0，同时记录下来在环中的位置，下次循环跳过该位置（模拟该小朋友退出游戏）
+     * 用一个count记录小朋友的数量，每次数到m就-1，小于n时就退出循环
+     */
+    public static int LastRemaining_Solution(int n, int m) {
+        if (n < 1 || m < 1)
+            return -1;
+        int[] array = new int[n];
+        int i = -1;//数组的指针
+        int step = 0;
+        int count = n;
+        while (count > 0) {
+            i++;
+            if (i == n) {//模拟数组是一个环
+                i = 0;
+            }
+            if (array[i] == -1) {//剔除数到m的小朋友
+                continue;
+            }
+            step++;//被剔除的小朋友不能数了，所以在之后++
+            if (step == m) {
+                step = 0;
+                array[i] = -1;//记录位置
+                count--;//小朋友数量-1
+            }
+        }
+        //返回跳出循环时的i,即最后一个被设置为-1的元素
+        return i;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
